735306fc823fe0314c2eb6c405d95ebcf557a177
erik-a-bensen <bensen.erik.a@gmail.com>
2025-08-10 14:47:26 -0600

    more pydoc debugging

--- src/mpitools/__init__.py
+++ src/mpitools/__init__.py
@@ -16,20 +16,20 @@ from .quick_comms import (
     reduce_to_all
 )
 
-__all__ = [
-    'setup_mpi',
-    'abort_on_error',
-    'eval_on_main',
-    'eval_on_workers',
-    'eval_on_single',
-    'eval_on_select',
-    'broadcast_from_main',
-    'broadcast_from_process',
-    'gather_to_main',
-    'gather_to_process',
-    'gather_to_all',
-    'reduce_to_main',
-    'reduce_to_process',
-    'reduce_to_all'
-]
+# __all__ = [
+#     'setup_mpi',
+#     'abort_on_error',
+#     'eval_on_main',
+#     'eval_on_workers',
+#     'eval_on_single',
+#     'eval_on_select',
+#     'broadcast_from_main',
+#     'broadcast_from_process',
+#     'gather_to_main',
+#     'gather_to_process',
+#     'gather_to_all',
+#     'reduce_to_main',
+#     'reduce_to_process',
+#     'reduce_to_all'
+# ]
 
--- src/mpitools/queue/__init__.py
+++ src/mpitools/queue/__init__.py
@@ -1,4 +1,4 @@
 from .tasks import Task, TaskResult 
 from .managers import MPIQueue
 
-__all__ = ["Task", "TaskResult", "MPIQueue"]
+# __all__ = ["Task", "TaskResult", "MPIQueue"]
