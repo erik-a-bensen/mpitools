40735c4965f04595120a95c0c0f59e5f518acece
erik-a-bensen <bensen.erik.a@gmail.com>
2025-08-12 23:51:44 -0600

    added buffered variable length communications

--- src/mpitools/comms/__init__.py
+++ src/mpitools/comms/__init__.py
@@ -13,6 +13,29 @@ from .reduction import (
     reduce_to_process,
     reduce_to_all
 )
+from .buffered_collective import (
+    buffered_broadcast_from_main,
+    buffered_broadcast_from_process,
+    buffered_scatter_from_main,
+    buffered_scatter_from_process,
+    buffered_gather_to_main,
+    buffered_gather_to_process,
+    buffered_gather_to_all,
+    buffered_all_to_all
+)
+from .buffered_reduction import (
+    buffered_reduce_to_main,
+    buffered_reduce_to_process,
+    buffered_reduce_to_all
+)
+from .variable_collective import (
+    variable_scatter_from_main,
+    variable_scatter_from_process,
+    variable_gather_to_main,
+    variable_gather_to_process,
+    variable_gather_to_all,
+    variable_all_to_all,
+)
 
 __all__ = [
     'broadcast_from_main',
@@ -25,5 +48,22 @@ __all__ = [
     'all_to_all',
     'reduce_to_main',
     'reduce_to_process',
-    'reduce_to_all'
+    'reduce_to_all',
+    'buffered_broadcast_from_main',
+    'buffered_broadcast_from_process',
+    'buffered_scatter_from_main',
+    'buffered_scatter_from_process',
+    'buffered_gather_to_main',
+    'buffered_gather_to_process',
+    'buffered_gather_to_all',
+    'buffered_all_to_all',
+    'buffered_reduce_to_main',
+    'buffered_reduce_to_process',
+    'buffered_reduce_to_all',
+    'variable_scatter_from_main',
+    'variable_scatter_from_process',
+    'variable_gather_to_main',
+    'variable_gather_to_process',
+    'variable_gather_to_all',
+    'variable_all_to_all',
 ]
